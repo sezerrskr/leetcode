6c2e0cc0ffac44988d2085c45180c77fa0e6b6c8
Sezer Seker <65251959+sezerrskr@users.noreply.github.com>
2026-02-16 06:20:32 +0300

    Time: 5 ms (59.43%), Space: 58.4 MB (37.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,34 +2,28 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
-    let rows = new Map();
-    let cols = new Map();
-    let boxes = new Map();
+var isValidSudoku = function(board) {
+    const rows = new Array(9).fill().map(() => new Set());
+    const cols = new Array(9).fill().map(() => new Set());
+    const boxes = new Array(9).fill().map(() => new Set());
 
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             let value = board[r][c];
 
-            if (value == ".") {
-                continue
+            if (value === ".") continue;
+
+            let boxId = (Math.floor(r / 3) * 3) + Math.floor(c / 3);
+
+            if (rows[r].has(value) || cols[c].has(value) || boxes[boxId].has(value)) {
+                return false;
             }
 
-            let boxId = Math.floor(r / 3) * 3 + Math.floor(c / 3);
-
-            if (!rows.has(r)) rows.set(r, new Set())
-            if (!cols.has(c)) cols.set(c, new Set())
-            if (!boxes.has(boxId)) boxes.set(boxId, new Set())
-            if (rows.get(r).has(value) || cols.get(c).has(value) || boxes.get(boxId).has(value)) {
-                return false
-            }
-
-            rows.get(r).add(value);
-            cols.get(c).add(value);
-            boxes.get(boxId).add(value);
-            // Eğer bu üçünden biri bile undefined ise, bir sonraki satırda patlayacaksın demektir.
-            console.log(`Bakıyorum -> R:${r} C:${c} | Mapler ->`, rows.get(r), cols.get(c), boxes.get(boxId));
+            // Ekle
+            rows[r].add(value);
+            cols[c].add(value);
+            boxes[boxId].add(value);
         }
     }
-    return true
+    return true;
 };
